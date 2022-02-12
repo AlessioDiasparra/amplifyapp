a47cccf6ca41d69a9972bbb98699f2d2993c0a66
Alessio Diasparra <alessiodiasp@libero.it>
2022-02-12 16:01:08 +0100

    Aggiunte API, query GRAPHQL e servizio di Storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,26 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "S3Trigger46b3c3d4": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s3e1f48c75": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
